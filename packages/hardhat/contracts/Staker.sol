d921e85043c8894704ca68abfe97c4b4b18cf44e
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-02-06 10:30:50 +0530

    finished checkpoint 3

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -6,10 +6,56 @@ import "./ExampleExternalContract.sol";
 contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
+  mapping( address => uint256) public balances;
+  bool public openForWithdraw;
+  uint256 public constant threshold = 1 ether;
+  uint256 public deadline = block.timestamp + 40 seconds;
+  event Stake(address staker, uint256 balance);
+  
 
   constructor(address exampleExternalContractAddress) public {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
+
+  function stake() public payable  {
+    emit Stake(msg.sender, msg.value);
+    balances[msg.sender] = balances[msg.sender] + msg.value;
+
+  }
+
+  function timeLeft() public view returns(uint256) {
+    if(block.timestamp >= deadline){
+      return 0;
+    }
+    return deadline;
+  }
+
+  function execute() public {
+    //require(timeLeft() > 0 );
+    if(timeLeft() > 0 && address(this).balance >= threshold ){
+      exampleExternalContract.complete{value: address(this).balance}();
+      openForWithdraw = false;
+      //deadline = block.timestamp + 40 seconds;
+    } else if (address(this).balance <= threshold){
+      openForWithdraw = true;
+    }
+  }
+
+  function withdraw(address payable gucci) public {
+    require(openForWithdraw == true && timeLeft() == 0);
+    address payable owner;
+    if(balances[msg.sender] != 0){
+      owner = payable(msg.sender);
+    }
+    owner.transfer(balances[gucci]);
+    openForWithdraw = false;
+
+  }
+
+  
+
+  
+
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
